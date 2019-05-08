94f81fe0ed26ad876f1b717ff774ff0c42e28ca1
hilenet <illogical.j@gmail.com>
2019-05-08 22:02:02 +0900

    feat: とりあえずfragment+viewmodel

--- app/src/main/java/com/example/yatterdroid/MainActivity.kt
+++ app/src/main/java/com/example/yatterdroid/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.yatterdroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.yatterdroid.ui.timeline.TimelineFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // appContainerに代入
+        // mainActivityの一要素として，飽くまでContainerの中
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.appContainer, TimelineFragment.newInstance())
+                .commitNow()
+        }
     }
 }
